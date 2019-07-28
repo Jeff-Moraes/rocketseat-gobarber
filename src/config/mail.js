c667247e20366576e5dd7a6e9459927518eac6f1
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-07-28 10:51:57 +0200

    feature: Environment Variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'e6e671a26f659f',
-    pass: '7a529213ad4777',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://45256c2ff2fb4106ab52842baeffb333@sentry.io/1515291',
+  dsn: process.env.SENTRY_DSN,
 };
